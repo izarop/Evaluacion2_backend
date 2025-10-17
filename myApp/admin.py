85cffcf81491583ba366215bef553aece89c766a
izarop <elisa.oyarzun02@inacapmail.cl>
2025-10-17 20:48:31 -0300

    Actualización: ahora si que si

--- myApp/admin.py
+++ myApp/admin.py
@@ -3,4 +3,7 @@ from .models import Categoria, Producto
 # Register your models here.
 
 admin.site.register(Categoria)
-admin.site.register(Producto)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):   
+    list_display = ( 'nombre_producto', 'precio', 'stock', 'descripcion')
